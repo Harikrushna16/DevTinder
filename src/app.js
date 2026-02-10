6dfdba333db639d10107755be0417daa7990e6fd
HarikrushnaDDS <harikrushnadds@gmail.com>
2026-02-10 11:21:12 +0530

    Explored HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,13 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("Hello! Hello! Hello!");
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Harikrushna", lastName: "DDS" })
 });
 
-app.use("/test", (req, res) => {
-    res.send("Hello from the server to /test!");
-});
+app.post("/user", (req, res) => {
+    res.send("User created successfully");
+})
+
+app.patch("/user", (req, res) => {
+    res.send("User updated successfully");
+})
+
+app.put("/user", (req, res) => {
+    res.send("User updated successfully");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted successfully");
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
