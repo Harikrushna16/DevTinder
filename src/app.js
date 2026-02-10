14cc30ec1a585a2546b2d6c66c7680ea23fd2bf8
HarikrushnaDDS <harikrushnadds@gmail.com>
2026-02-10 17:48:40 +0530

    added admin auth and user auth middleware

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
-const { adminAuth } = require("./middleware/auth");
+const { adminAuth, userAuth } = require("./middleware/auth");
 
 const app = express();
 
-app.use("/user", adminAuth);
+app.use("/user", userAuth);
 
 app.get("/user", (req, res) => {
-    res.send({ firstName: "Harikrushna", lastName: "DDS" })
+    res.send({ firstName: "User", lastName: "DDS" })
 });
 
 app.post("/user", (req, res) => {
     res.send("User created successfully");
 })
+
+app.get("/admin", adminAuth, (req, res) => {
+    res.send({ firstName: "Admin", lastName: "DDS" })
+});
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,5 @@
 const adminAuth = (req, res, next) => {
+    console.log("Admin auth middleware checked!");
     const token = "xyz";
     const isAdminAuthenticated = "xyz" === token;
     if (!isAdminAuthenticated) {
@@ -8,4 +9,15 @@ const adminAuth = (req, res, next) => {
     }
 }
 
-module.exports = { adminAuth };
+const userAuth = (req, res, next) => {
+    console.log("User auth middleware checked!");
+    const token = "xyz";
+    const isUserAuthenticated = "xyz" === token;
+    if (!isUserAuthenticated) {
+        res.status(401).send("You are not authorized to access this resource");
+    } else {
+        next();
+    }
+}
+
+module.exports = { adminAuth, userAuth };
