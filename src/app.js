aea9c8fbefa87c86f56f42cb6f784614a0961c7b
HarikrushnaDDS <harikrushnadds@gmail.com>
2026-02-12 09:45:33 +0530

    create /signup api and store user to DB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
+// middleware to parse json
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        res.status(400).json({ message: "User registration failed", error });
+    }
+});
 
 connectDB()
     .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://harry:harry1234@cluster0.4vfc5ea.mongodb.net/?appName=Cluster0");
+    await mongoose.connect("mongodb+srv://harry:harry1234@cluster0.4vfc5ea.mongodb.net/devTinder?appName=Cluster0");
 }
 
 module.exports = connectDB;
