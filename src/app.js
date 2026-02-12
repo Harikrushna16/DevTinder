20ce498331e4caa8ab4880b6b1476a52b93ad504
HarikrushnaDDS <harikrushnadds@gmail.com>
2026-02-12 08:20:35 +0530

    added databse connections

--- src/app.js
+++ src/app.js
@@ -1,39 +1,13 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middleware/auth");
-
 const app = express();
+const connectDB = require("./config/database");
 
-app.use("/user", userAuth);
-
-app.get("/user", (req, res) => {
-    try {
-        res.send({ firstName: "User", lastName: "DDS" })
-    } catch (error) {
+connectDB()
+    .then(() => {
+        console.log("MongoDB connected");
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+    }).catch((error) => {
         console.log(error);
-        res.status(500).send("Internal server error");
-    }
-});
-
-app.post("/user", (req, res) => {
-    try {
-        res.send("User created successfully");
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal server error!!!");
-    }
-})
-
-app.get("/admin", adminAuth, (req, res) => {
-    try {
-        throw new Error("Something went wrong");
-        res.send({ firstName: "Admin", lastName: "DDS" })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal server error!!!!");
-    }
-});
-
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+    });
---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://harry:harry1234@cluster0.4vfc5ea.mongodb.net/?appName=Cluster0");
+}
+
+module.exports = connectDB;
+
