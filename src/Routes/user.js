f95067e6c2f88c20dcf31b728b0e4b79fc0d3cb0
HarikrushnaDDS <harikrushnadds@gmail.com>
2026-02-18 18:20:27 +0530

    added /connections api

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -9,7 +9,7 @@ userRouter.get("/requests/received", userAuth, async (req, res) => {
         const requests = await RequestConnection.find({
             toUserId: loggedInUserId,
             status: "interested"
-        }).populate("fromUserId", "firstName lastName email profilePicture");
+        }).populate("fromUserId", "firstName lastName profilePicture");
         if (!requests) {
             return res.status(404).json({ message: "No requests found" });
         }
@@ -18,5 +18,33 @@ userRouter.get("/requests/received", userAuth, async (req, res) => {
         res.status(400).json({ message: error.message || "Failed to get requests" });
     }
 });
+
+userRouter.get("/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const connections = await RequestConnection.find({
+            $or: [{
+                fromUserId: loggedInUser._id,
+                status: "accepted"
+            }, {
+                toUserId: loggedInUser._id,
+                status: "accepted"
+            }]
+        }).populate("fromUserId", "firstName lastName profilePicture").populate("toUserId", "firstName lastName profilePicture");
+        if (!connections) {
+            return res.status(404).json({ message: "No connections found" });
+        }
+        const formattedConnections = connections.map((connection) => {
+            if (connection.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return connection.toUserId;
+            } else {
+                return connection.fromUserId;
+            }
+        })
+        return res.status(200).json({ formattedConnections, message: "Connections fetched successfully" });
+    } catch (error) {
+        res.status(400).json({ message: error.message || "Failed to get connections" });
+    }
+})
 
 module.exports = userRouter;
